abbc4e1ad0a72320d25348adac13b49938b7f015
Ayden-Kilpatrick <123522556+Ayden-Kilpatrick@users.noreply.github.com>
2024-03-07 19:26:54 -0600

    Update BMIcalc.py

--- BMIcalc.py
+++ BMIcalc.py
@@ -4,10 +4,10 @@ print("Enter you wieght in pounds")
 height = float(input())
 x = weight/float(height*height)
 if x < 18.5:
-    print('Under weight')
+    print("BMI = ",x,'Under weight')
 if x>=18.5 and x<25:
-    print("Normal weight")
+    print("BMI = ",x,"Normal weight")
 if x >= 25 and x < 30:
-   print('Over weight')
+   print("BMI = ",x,'Over weight')
 if x >= 30:
-   print('Obese')
+   print("BMI = ",x,'Obese')
