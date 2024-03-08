281b856773e70fc28ef817464abf1b078e1facfe
Ayden-Kilpatrick <123522556+Ayden-Kilpatrick@users.noreply.github.com>
2024-03-07 18:39:38 -0600

    Add files via upload

---
+++ BMIcalc.py
@@ -0,0 +1,13 @@
+print("Enter your hieght in inches")
+weight = int(input())
+print("Enter you wieght in pounds")
+height = float(input())
+x = weight/float(height*height)
+if x < 18.5:
+    print('Underweight')
+if x>=18.5 and x<25:
+    print("Normal")
+if x >= 25 and x < 30:
+   print('Overweight')
+if x >= 30:
+   print('Obesity')
